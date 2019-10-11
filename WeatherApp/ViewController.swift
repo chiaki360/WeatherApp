13306a3be14b82f8cc43cf90319f9196e218d527
Chiaki <is0529vp@ed.ritsumei.ac.jp>
2019-10-11 13:50:10 +0900

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Chiaki Yuge on 2019/10/11.
+//  Copyright © 2019 Chiaki Yuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
