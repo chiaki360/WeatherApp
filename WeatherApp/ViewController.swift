416705050732e331befc16ebdeabca0b4e1053ee
Shaiful Nizam <is0535fs@ed.ritsumei.ac.jp>
2019-10-16 14:32:28 +0900

    Project Day 1

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let items = ["Japan","China","South Korea", "Indonesia", "Malaysia"]
+        for item in items {
+            countries.append(item)
+        }
+        print(countries)
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "City") as? CityTableViewController {
+            // 2: success! Set its selectedImage property
+            //vc.selectedImage = countries[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
